71f30a2103751b327d573861baf334e140775b70
Christian <Cdzhokov6094@stu.d214.org>
2024-04-17 17:45:53 -0500

    ugycfuyiogcfh

--- ContentView.swift
+++ ContentView.swift
@@ -6,11 +6,6 @@ struct ContentView: View {
     @State var number2: Int = 0
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("number 2", value: $number2, format: .number)
     }
 }
